6f32952d89a4f35a06131ea0f68de589c91d38f6
ritik1jain <ritikjain0706.com>
2021-04-29 14:30:27 +0530

    Adding Enter keypress functionality to searchbox

--- src/components/search/Searchbox.js
+++ src/components/search/Searchbox.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Redirect} from 'react-router-dom';
+import Apicall from './apicall';
 
 const Searchbox = (props) => {
+    const [flag, setFlag] = useState(false);
+    const handleKeyPress =async e => {
+        if(e.key === 'Enter'){
+            await Apicall(props.state,props.onChange);
+            setFlag(true);
+        } 
+    }
+
+    if(flag) {
+        return <Redirect to="/search-result" />
+    }
+    
 
     return (
         <div className="seach-box">
@@ -8,7 +22,7 @@ const Searchbox = (props) => {
                 <span className="input-search">
                     <img src="assets/images/icons/search-icon.png" />
                 </span>
-                <input type="text" value={props.state.keyword} onChange={(e) => props.onChange({...props.state, keyword: e.target.value})} className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" />
+                <input type="text" value={props.state.keyword} onChange={(e) => props.onChange({...props.state, keyword: e.target.value})} onKeyPress={handleKeyPress} className="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" />
             </div>
         </div>
 
